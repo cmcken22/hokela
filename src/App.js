d90b6fe8b8085f2de30851a3e09a0343e17565eb
Conner McKenna <cmckenna@ellisdon.com>
2020-05-09 17:55:36 -0400

    signing custom access token

--- src/App.js
+++ src/App.js
@@ -2,48 +2,61 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import cookies from 'react-cookies';
 import cx from 'classnames';
+import jwt_decode from 'jwt-decode';
 
 import './App.scss';
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      isAdmin: false
     };
   }
 
   componentDidMount() {
     console.clear();
-    console.log('CLICK');
     const accessToken = cookies.load('accessToken');
-    this.setState({ loggedIn: !!accessToken });
+    console.log('accessToken:', accessToken);
+    this.setState({ loggedIn: !!accessToken }, () => this.getUserScopes(accessToken));
   }
 
-  handleClick = () => {
-    
+  getUserScopes = (accessToken) => {
+    console.log('getUserScopes:', accessToken);
+    if (accessToken) {
+      const { scopes } = jwt_decode(accessToken);
+      console.log('scopes:', scopes);
+      const isAdmin = scopes.some(scope => scope === 'ROLE_ADMIN');
+      console.log('isAdmin:', isAdmin);
+      this.setState({ isAdmin });
+    }
   }
 
   render() {
-    const { loggedIn } = this.state;
+    const { loggedIn, isAdmin } = this.state;
+    const scopes = [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/cloud-platform'
+    ]
     const gState = '123';
-    const scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
-    // const scope = 'https://www.googleapis.com/auth/v1/people/me';
-    // const scope = 'https://www.googleapis.com/plus/v1/people/me';
     const prompt = 'select_account';
     const includeGrantedScopes = false;
     const redirectUri = process.env.REDIRECT_URI_TOKEN;
     const responseType = 'code';
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&include_granted_scopes=${includeGrantedScopes}&response_type=${responseType}&state=${gState}&redirect_uri=${redirectUri}&client_id=${process.env.CLIENT_ID}&prompt=${prompt}`;
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scopes.join(' ')}&include_granted_scopes=${includeGrantedScopes}&response_type=${responseType}&state=${gState}&redirect_uri=${redirectUri}&client_id=${process.env.CLIENT_ID}&prompt=${prompt}`;
     console.log('URL:', url);
 
     return(
-      <div className={cx("app", "alwayshee", {
+      <div className={cx("app", {
         "app--logged-in": loggedIn
       })}> 
         Hello World!!!!
         <a href={url}>login</a>
+        {isAdmin && (
+          <p>YOU ARE AN ADMIN</p>
+        )}
       </div>
     );
   }
