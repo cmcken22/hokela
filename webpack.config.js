ec3696233fabf441c7c5485e49060f41ecfdef53
Conner McKenna <cmckenna@ellisdon.com>
2020-09-14 21:02:46 -0400

    updating web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env',"@babel/preset-react"],
+          plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime'],
+        }
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|otf|svg)$/, loader: 'url-loader'
