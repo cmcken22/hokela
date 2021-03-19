f2e2cd6d488ab6df2385545512b3029722f8da77
Conner McKenna <cmckenna@ellisdon.com>
2021-03-19 16:54:29 -0400

    Updates

---
+++ precache-manifest.7944c4093005cc21fcc19efe06a0c4c9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdec0af4378d211c74292dd17ef61e6",
+    "url": "/hokela/index.html"
+  },
+  {
+    "revision": "b34aa3879a314a02f8cc",
+    "url": "/hokela/static/css/2.f0db2a3b.chunk.css"
+  },
+  {
+    "revision": "4afa47df010170f9b3e7",
+    "url": "/hokela/static/css/main.342f1f63.chunk.css"
+  },
+  {
+    "revision": "b34aa3879a314a02f8cc",
+    "url": "/hokela/static/js/2.dad01ad5.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/hokela/static/js/2.dad01ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afa47df010170f9b3e7",
+    "url": "/hokela/static/js/main.3d2b5f7b.chunk.js"
+  },
+  {
+    "revision": "f33e3884ca107f342706",
+    "url": "/hokela/static/js/runtime-main.f937da06.js"
+  },
+  {
+    "revision": "c11d5f1df800a0a761757f520db1e1e1",
+    "url": "/hokela/static/media/Facebook.c11d5f1d.svg"
+  },
+  {
+    "revision": "baaf69f44b497489f1c0ac3063fcc933",
+    "url": "/hokela/static/media/Instagram.baaf69f4.svg"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/hokela/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/hokela/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/hokela/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/hokela/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "efebb08fe794a5e0fac38c58c03a2a38",
+    "url": "/hokela/static/media/LinkedIn.efebb08f.svg"
+  },
+  {
+    "revision": "a9efc0d96889659ea5bc98b7c0b32ed5",
+    "url": "/hokela/static/media/Location.a9efc0d9.svg"
+  },
+  {
+    "revision": "c82170e08b76657553ab939bd28e8515",
+    "url": "/hokela/static/media/Metropolis-Light.c82170e0.otf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/hokela/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  }
+]);
