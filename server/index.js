a117c46d5ed70b43d10c5ea0869f8170abdee369
Conner McKenna <cmckenna@ellisdon.com>
2021-03-22 23:15:12 -0400

    adding new relic

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('newrelic');
 
 const path = require('path');
 const config = require('./config');
