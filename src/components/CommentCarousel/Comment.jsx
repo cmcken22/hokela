f8310e4c56b8d7bc0e73ddabc901fe8ab3dbd696
Conner McKenna <cmckenna@ellisdon.com>
2021-08-18 17:36:21 -0400

    adding the full view modal to the comment carousel

--- src/components/CommentCarousel/Comment.jsx
+++ src/components/CommentCarousel/Comment.jsx
@@ -1,28 +1,86 @@
 import React, { useState, useRef, useEffect } from 'react';
 import cx from 'classnames';
 
-const Comment = ({ text, name, title, index }) => {
+const Comment = ({ text, name, title, index, onClick, large }) => {
   const [hasOverflow, setOverflow] = useState(false)
   const inputRef = useRef(null);
-  
+  let timer = null;
+
+  const detectOverflow = () => {
+    if (inputRef && inputRef.current && index === 1 && !large) {
+      if (!!timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      timer = setTimeout(() => {
+        const { current: element } = inputRef;
+        const overflow = element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+        if (!hasOverflow && overflow) setOverflow(true);
+        else setOverflow(false);
+      }, 200);
+    }
+  }
 
   useEffect(() => {
-    if (inputRef && inputRef.current) {
-      const { current: element } = inputRef;
-      const overflow = element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
-      if (!hasOverflow && overflow) setOverflow(true);
-      else setOverflow(false);
-    }
+    window.addEventListener('resize', detectOverflow);
+    return () => window.addEventListener('resize', detectOverflow);
   }, [text]);
 
+  useEffect(() => {
+    detectOverflow();
+  }, [text]);
+
+  const openLink = (link) => {
+    if (link) window.open(link, '_blank');
+  }
+
+  const renderUserInfo = () => {
+    return (
+      <>
+        <div className="comment__name">{name}</div>
+        {title && (
+          <div className="comment__title">{title}</div>
+        )}
+      </>
+    );
+  }
+
+  const renderHeader = () => {
+    if (large) {
+      return (
+        <div className="comment__header">
+          {renderUserInfo()}
+          <div className="comment__close-btn" onClick={onClick} />
+        </div>
+      );
+    }
+    return (
+      <div className="comment__header" />
+    );
+  }
+
+  const renderFooter = () => {
+    if (!large) return null;
+
+    return (
+      <div className="comment__footer">
+        <div className="comment__icon comment__icon--website" onClick={openLink("")} />
+        <div className="comment__icon comment__icon--facebook" onClick={openLink("")} />
+        <div className="comment__icon comment__icon--instagram" onClick={openLink("")} />
+        <div className="comment__icon comment__icon--linkedIn" onClick={openLink("")} />
+      </div>
+    );
+  }
+
   const handleClick = () => {
-    console.clear();
-    console.log('click');
+    if (onClick) onClick();
   }
 
   return (
-    <div className="comment">
-      <div className="comment__header"></div>
+    <div className={cx("comment", {
+      "comment--large": large
+    })}>
+      {renderHeader()}
       <div className="comment__text" ref={inputRef}>
         <p>{text}</p>
       </div>
@@ -36,11 +94,13 @@ const Comment = ({ text, name, title, index }) => {
         </div>
       )}
 
-      <div className="comment__divider" />
-      <div className="comment__name">{name}</div>
-      {title && (
-        <div className="comment__title">{title}</div>
+      {!large && (
+        <>
+          <div className="comment__divider" />
+          {renderUserInfo()}
+        </>
       )}
+      {renderFooter()}
     </div>
   );
 }
--- src/components/CommentCarousel/CommentCarousel.jsx
+++ src/components/CommentCarousel/CommentCarousel.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import cx from 'classnames';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import Comment from './Comment';
+import { Row, Col } from 'Components/Grid';
 
 const ANIMATION_DURATION = 500;
 const BASE_CONTAINER_HEIGHT = 434;
@@ -12,6 +13,7 @@ class CommentCarousel extends Component {
     this.state = {
       active: false,
       containerHeight: BASE_CONTAINER_HEIGHT,
+      fullView: false,
       content: [
         {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
@@ -46,7 +48,7 @@ class CommentCarousel extends Component {
   componentDidMount() {
     window.addEventListener('resize', this.handleResize);
   }
-  
+
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize);
   }
@@ -78,6 +80,26 @@ class CommentCarousel extends Component {
     }
   }
 
+  handleLeftArrowClick = () => {
+    const { fullView } = this.state;
+    if (fullView) {
+      const nextContent = this.shiftLeft();
+      return this.setState({ content: nextContent });
+    }
+
+    this.beginAnimation('left');
+  }
+
+  handleRightArrowClick = () => {
+    const { fullView } = this.state;
+    if (fullView) {
+      const nextContent = this.shiftRight();
+      return this.setState({ content: nextContent });
+    }
+
+    this.beginAnimation('right');
+  }
+
   shiftLeft = () => {
     const { content } = this.state;
     const nextContent = [...content];
@@ -95,7 +117,7 @@ class CommentCarousel extends Component {
     newArray.push(firstItem);
     return newArray;
   }
-  
+
   shiftRight = () => {
     const { content } = this.state;
     const nextContent = [...content];
@@ -125,25 +147,78 @@ class CommentCarousel extends Component {
 
   renderItem = (type, item, callback, index) => {
     const { text, name, title } = item;
+
     return (
       <div className={`item item__${type}`}>
         <div className="item__content" ref={callback ? (ref) => callback(type, ref) : undefined}>
           <Comment
             key={`comment--${index}`}
             index={index}
+            title={title}
             text={text}
             name={name}
-            title={title}
+            onClick={this.toggleFullView}
           />
         </div>
       </div>
     );
   }
 
-  render() {
-    const { active, content, containerHeight } = this.state;
+  renderLargeComments = (type, item, index) => {
+    const { fullView } = this.state;
+    const { text, name, title } = item;
+    if (!fullView) return null;
 
-    return(
+    return (
+      <div className="comment-carousel__large-container">
+        <Row>
+          <Col span={8} offset={2}>
+            <div className="content">
+              <Comment
+                key={`comment--${index}`}
+                index={index}
+                title={title}
+                text={text}
+                name={name}
+                onClick={this.toggleFullView}
+                large
+              />
+            </div>
+          </Col>
+        </Row>
+        {this.renderArrows()}
+      </div>
+    );
+  }
+
+  renderArrows = () => {
+    return (
+      <>
+        <div
+          onClick={this.handleLeftArrowClick}
+          className="comment-carousel__button comment-carousel__button--left"
+        >
+          <LeftOutlined style={{ color: 'white', fontSize: 16 }} />
+        </div>
+        <div
+          onClick={this.handleRightArrowClick}
+          className="comment-carousel__button comment-carousel__button--right"
+        >
+          <RightOutlined style={{ color: 'white', fontSize: 16 }} />
+        </div>
+      </>
+    );
+  }
+
+  toggleFullView = () => {
+    const { fullView } = this.state;
+    this.setState({ fullView: !fullView });
+  }
+
+  render() {
+    const { active, content, containerHeight, fullView } = this.state;
+
+    return (
       <div className="comment-carousel-container">
         <div
           className={cx("comment-carousel", {
@@ -154,6 +229,7 @@ class CommentCarousel extends Component {
             height: `${containerHeight}px`
           }}
         >
+          {this.renderLargeComments('main', content[1], 1)}
           <div className="content">
             {content[0] && this.renderItem('prev', content[0], this.createRef, 0)}
             {content[1] && this.renderItem('main', content[1], this.createRef, 1)}
@@ -161,18 +237,7 @@ class CommentCarousel extends Component {
             {content[3] && this.renderItem('last', content[3], this.createRef, 3)}
           </div>
         </div>
-        <div
-          onClick={() => this.beginAnimation('left')}
-          className="comment-carousel__button comment-carousel__button--left"
-        >
-          <LeftOutlined style={{ color: 'white', fontSize: 16 }} />
-        </div>
-        <div
-          onClick={() => this.beginAnimation('right')}
-          className="comment-carousel__button comment-carousel__button--right"
-        >
-          <RightOutlined style={{ color: 'white', fontSize: 16 }} />
-        </div>
+        {!fullView && this.renderArrows()}
       </div>
     );
   }
